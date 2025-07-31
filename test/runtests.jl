c44558a15a2e3c4976eb625fea502c6b687eb5f5
ChrisRackauckas <accounts@chrisrackauckas.com>
2025-07-31 04:47:36 -0400

    [ci-skip] Revert "Apply JuliaFormatter to fix code formatting"
    This reverts commit 590009a.
    
    Reverting JuliaFormatter changes due to formatting issues being addressed in JuliaFormatter.jl PR#933.

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,7 +9,7 @@ using Test: @test
         good_exprs = [
             :(@hello),
             Meta.parse("@hello"),
-            Meta.parse("@hello()") # Is this correct?
+            Meta.parse("@hello()"), # Is this correct?
         ]
         bad_exprs = [
             Meta.parse("@foo bar"),
@@ -17,7 +17,7 @@ using Test: @test
             Meta.parse("foo()"),
             Meta.parse("foo(@bar)"),
             Meta.parse("@foo @bar"),
-            Meta.parse("@foo(@bar)")
+            Meta.parse("@foo(@bar)"),
         ]
         for expr in good_exprs
             @test SciMLPublic._is_valid_macro_expr(expr)
@@ -47,7 +47,7 @@ function _public_names(mod::Module)
     result = Base.names(
         mod;
         all = false,
-        imported = false
+        imported = false,
     )
     return result
 end
